0d71d7e06b5ae253321df178a827f197ebd230a9
Paul Iakab <paul.iakab@globant.com>
2020-05-31 01:09:49 +0300

    Implement the pagination

--- src/components/book-card/book-card.component.js
+++ src/components/book-card/book-card.component.js
@@ -8,17 +8,19 @@ const BookCard = ({ author, title, description, deleteBook, id }) => {
   return (
     <div className="card mb-3">
       <div className="row no-gutters">
-        <div className="col-md-3">
+        <div className="col-md-3 my-col">
           <img src={require('assets/img/book.png')} className="card-img" alt="..." />
         </div>
-        <div className="col-md-8">
+        <div className="col-md-8 my-col">
           <div className="card-body">
-            <div className="my-card-header">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+            <div className="my-card-content">
+              <div className="my-card-header">
+                <h5 className="card-title">{title}</h5>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+              <h6 className="text-muted">{author}</h6>
+              <p className="card-text">{description}</p>
             </div>
-            <h6 className="text-muted">{author}</h6>
-            <p className="card-text">{description}</p>
             <div className="my-button-container">
               <button type="button" className="btn btn-danger" aria-label="Close" onClick={handleDelete}>
                 <span aria-hidden="true">Delete</span>
--- src/components/books-list/books-list.component.js
+++ src/components/books-list/books-list.component.js
@@ -1,33 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import BookCard from 'components/book-card/book-card.component';
 
 import './book-list.style.scss';
 
-const renderPagination = () => (
-  <ul className="pagination justify-content-center ">
-    <li className="page-item disabled">
-      <a className="page-link" href="#" tabIndex="-1" aria-disabled="true">Previous</a>
-    </li>
-    <li className="page-item"><a className="page-link" href="#">1</a></li>
-    <li className="page-item active" aria-current="page">
-      <a className="page-link" href="#">2 <span className="sr-only">(current)</span></a>
-    </li>
-    <li className="page-item"><a className="page-link" href="#">3</a></li>
-    <li className="page-item">
-      <a className="page-link" href="#">Next</a>
-    </li>
-  </ul>
-);
+const BooksList = ({ books = [], deleteBook }) => {
+  const [currentPage, setCurrentPage] = useState(1);
 
-const BooksList = ({ books = [], deleteBook }) => (
-  <div className="col">
-    <div className="list-group">
-      {books.map(({ title, author, description, id }) => (
-        <BookCard title={title} author={author} description={description} deleteBook={deleteBook} id={id} key={id} />
-      ))}
+  useEffect(() => {
+    setCurrentPage(1);
+  }, [books])
+
+  const getPaginationItems = noOfPages => {
+    const items = [];
+    for(let i = 1; i <= noOfPages; i++) {
+      items.push(<li className={`page-item ${currentPage === i && 'active'}`}><a className="page-link">{i}</a></li>)
+    }
+
+    return items;
+  };
+
+  const handleNextPage = () => setCurrentPage(currentPage + 1);
+  const handlePreviousPage = () => setCurrentPage(currentPage - 1);
+
+  const renderPagination = (noOfItems) => {
+    let noOfPages = Math.ceil(noOfItems / 3)
+
+    if (noOfPages < 2) return;
+
+    return (
+      <ul className="pagination justify-content-center my-pagination">
+        <li className={`page-item ${currentPage === 1 && 'disabled'}`}>
+          <button
+            className="page-link"
+            disabled={currentPage === 1}
+            onClick={handlePreviousPage}
+          >Previous</button>
+        </li>
+        {getPaginationItems(noOfPages)}
+        <li className={`page-item ${currentPage === noOfPages && 'disabled'}`}>
+          <button
+            disabled={currentPage === noOfPages}
+            className={"page-link"}
+            onClick={handleNextPage}
+          >Next</button>
+        </li>
+      </ul>
+    );
+  }
+
+  return (
+    <div className="col">
+      <div className="list-group my-list-group">
+        {books.slice((currentPage - 1) * 3, currentPage * 3).map(({ title, author, description, id }) => (
+          <BookCard title={title} author={author} description={description} deleteBook={deleteBook} id={id} key={id} />
+        ))}
+      </div>
+      {renderPagination(books.length)}
     </div>
-  {renderPagination()}
-  </div>
-);
+  );
+};
 
 export default BooksList;
